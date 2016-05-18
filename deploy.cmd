b62846e3585a126cc6063851a5aa722a4c739958
Jess Jakka <jjakka@gmail.com>
2016-05-18 14:30:01 -0400

    Polymer Init

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,1 @@
-@echo off
-start /w goapp deploy default-client-side/app.yaml
-start /w goapp deploy server-side/app.yaml
-start /w appcfg.py -A russel-watsky-website update_dispatch .
+goapp deploy default-client-side/app.yaml && goapp deploy server-side/app.yaml && appcfg.py -A russel-watsky-website update_dispatch .
